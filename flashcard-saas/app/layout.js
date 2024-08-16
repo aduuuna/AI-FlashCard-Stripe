82c60b294d5a849769bd1eafdca0448709dae9df
Owusu Ansah Joy <owusujoyansah@gmail.com>
2024-08-16 22:35:04 +0000

    Added Vercel analytics

--- flashcard-saas/app/layout.js
+++ flashcard-saas/app/layout.js
@@ -1,10 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          {children}
+          <Analytics />
+        </body>
       </html>
     </ClerkProvider>
   );
